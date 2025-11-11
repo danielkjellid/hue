00603cc5c242f69b879c424e81b987a611c7a1c1
Daniel Kjellid <42611331+danielkjellid@users.noreply.github.com>
2025-11-11 10:03:21 +0100

    Organize types

---
+++ hue-python/src/hue/types/css.py
@@ -0,0 +1,17 @@
+from typing import Literal
+
+type JustifyContent = Literal[
+    "justify-start",
+    "justify-center",
+    "justify-end",
+    "justify-between",
+    "justify-around",
+    "justify-evenly",
+]
+type AlignItems = Literal[
+    "items-start",
+    "items-center",
+    "items-end",
+    "items-stretch",
+    "items-baseline",
+]
