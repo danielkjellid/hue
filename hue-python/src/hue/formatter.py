55b227ac61955b7b213ac44a5ff475806cae8b84
Daniel Kjellid <42611331+danielkjellid@users.noreply.github.com>
2025-11-17 10:08:21 +0100

    Better mapping in formatter

--- hue-python/src/hue/formatter.py
+++ hue-python/src/hue/formatter.py
@@ -2,6 +2,8 @@ from typing import Any, Callable
 
 from htmy import Formatter
 
+from hue.types.core import AlpineAjaxProps
+
 
 class HueFormatter(Formatter):
     def __init__(
@@ -16,37 +18,7 @@ class HueFormatter(Formatter):
 
     def _format_name(self, name: str) -> str:
         # We're dealing with an alpine atribute, so it needs special handling.
-        if name.startswith("x_"):
-            return self.format_alpine_attr(name)
+        if name.startswith(str(AlpineAjaxProps.identifier)):
+            return AlpineAjaxProps.to_html(name)
         else:
             return super()._format_name(name)
-
-    def format_alpine_attr(self, name: str) -> str:
-        """
-        Format a Python attribute name to an Alpine.js compatible attribute.
-        Examples:
-        - 'x_on_click' -> 'x-on:click'
-        - 'x_on_click__outside' -> 'x-on:click.outside'
-        - 'x_transition__enter_start' -> 'x-transition:enter-start'
-        """
-        # First split by double underscore to separate modifiers
-        base_name, *modifiers = name.split("__")
-
-        if base_name == "x_data":
-            return "x-data"
-        elif base_name.startswith("x_on_"):
-            result = f"x-on:{base_name[5:].replace('_', '-')}"
-        elif base_name.startswith("x_transition_"):
-            result = f"x-transition:{base_name[13:].replace('_', '-')}"
-        elif base_name.startswith("x_bind_"):
-            result = f"x-bind:{base_name[7:].replace('_', '-')}"
-        elif base_name.startswith("x_"):
-            result = f"x-{base_name[2:].replace('_', '-')}"
-        else:
-            result = f"x-{base_name.replace('_', '-')}"
-
-        # Add any modifiers, converting underscores to hyphens
-        if modifiers:
-            result += "." + ".".join(mod.replace("_", "-") for mod in modifiers)
-
-        return result
